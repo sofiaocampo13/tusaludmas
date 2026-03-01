bbf13a2b176fb59ee50d6d96c648d4a2c83d4588
dggi1507 <giraldogiraldodaniel3@gmail.com>
2026-02-28 21:14:28 -0500

    Update user.js

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -17,6 +17,15 @@ const User = {
             JOIN roles r ON u.roles_id = r.id 
             WHERE u.link_code = ? AND u.state = 1`;
         db.query(sql, [code], callback);
+    },
+
+    // ESTE ES EL MÉTODO QUE LISTA TODOS LOS USUARIOS, INCLUYENDO SU ROL
+    findAll: (callback) => {
+        const sql = `
+            SELECT u.id, u.username, u.email, u.state, r.name as role_name 
+            FROM users u 
+            JOIN roles r ON u.roles_id = r.id`;
+        db.query(sql, callback);
     }
 };
 
