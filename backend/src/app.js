110efa73a1151513c5acb732383767637f6d434d
Jeronimo Sepulveda <jeronimosepulveda19@gmail.com>
2026-04-04 11:06:31 -0500

    Api EPS

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,7 @@ import dataRoutes from './routes/dataRoutes.js';
 import patientRoutes from './routes/patientRoutes.js';
 import appointmentRoutes from './routes/appointmentRoutes.js';
 import alarmRoutes from './routes/alarmRoutes.js';
-
+import externalRoutes from './routes/externalRoutes.js';
 const app = express();
 
 // Middlewares
@@ -22,6 +22,7 @@ app.use('/api/patients', patientRoutes);
 app.use('/api/appointments', appointmentRoutes);
 app.use('/api/alarms', alarmRoutes);
 
+app.use('/api/external', externalRoutes);
 // Ruta de prueba
 app.get('/api/saludo', (req, res) => {
   res.json({ mensaje: "Conexión exitosa desde el Backend de Node.js" });
